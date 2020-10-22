fa252d73ef954290d3cde804227a356a7823ba1d
takuyakasagawa <kasa.kasa.0062362@gmail.com>
2020-10-22 14:44:56 +0900

    edited js

--- src/lib/PasswordValidator.js
+++ src/lib/PasswordValidator.js
@@ -5,11 +5,17 @@ export default class extends BaseValidator {
     super(val, 'パスワード', 'password');
     this._checkLength = this._checkLength.bind(this);
     this._checkFormat = this._checkFormat.bind(this);
+    this._checkAlphabet = this._checkAlphabet.bind(this);
+    this._checkSymbol = this._checkSymbol.bind(this);
+    this._checkNumber = this._checkNumber.bind(this);
   }
   validate() {
     return super._cannotEmpty()
       .then(this._checkLength)
       .then(this._checkFormat)
+      .then(this._checkAlphabet)
+      .then(this._checkSymbol)
+      .then(this._checkNumber)
       .then((res) => {
         return { success: true }; // Promise.resolve({ success: true })と同一
       })
@@ -28,16 +34,60 @@ export default class extends BaseValidator {
       });
     } 
   }
+
+  //    const re = /^(?=.*?[A-Z])(?=.*?[0-9])(?=.*[.|@|_|-])[A-Za-z0-9|.@_-]*$/;
   _checkFormat() {
-    const re = /^(?=.*?[A-Z])(?=.*?[0-9])(?=.*[.|@|_|-])[A-Za-z0-9|.@_-]*$/;
+    const re = /^[A-Za-z0-9.@_-]*$/;
     const match = re.test(this.val);
     if (match) {
       return Promise.resolve();
-    } else {
+      }else {
         return Promise.reject({
           success: false,
           type: this.type,
-          message: `${this.type}のフォーマットが異なります。`
+          message: `${this.typeName}は英数字と記号@._-のみが使用できます。`
+        })
+      }
+  }
+
+  _checkAlphabet() {
+    const re = /[A-Z]/;
+    const match = re.test(this.val);
+    if (match) {
+      return Promise.resolve();
+      }else {
+        return Promise.reject({
+          success: false,
+          type: this.type,
+          message: `大文字英字をパスワードに入力してください。`
+        })
+      }
+  }
+
+  _checkSymbol() {
+    const re = /[_.-@]/i;
+    const match = re.test(this.val);
+    if (match) {
+      return Promise.resolve();
+      }else {
+        return Promise.reject({
+          success: false,
+          type: this.type,
+          message: `@._-の記号をどれか1文字以上入力してください。`
+        })
+      }
+  }
+
+  _checkNumber(){
+    const re = /[0-9]/;
+    const match = re.test(this.val);
+    if (match) {
+      return Promise.resolve();
+      }else {
+        return Promise.reject({
+          success: false,
+          type: this.type,
+          message: `数字を1文字以上入力してください。`
         })
       }
   }
